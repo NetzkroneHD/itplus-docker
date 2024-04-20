cf9982ac839c6fea40438a1a26888921812af962
NetzkroneHD <noah_01@gmx.de>
2024-04-20 21:36:30 +0200

    Lösungen hinzugefügt

--- itplus-docker-simple/src/main/java/de/netzkronehd/itplus/Main.java
+++ itplus-docker-simple/src/main/java/de/netzkronehd/itplus/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        if(Runtime.version().feature() == 16) {
+        if(Runtime.version().feature() >= 17) {
             while (true) {
                 Thread.sleep(1000);
                 System.out.println("Yaayy du benutz "+Runtime.version().feature());
